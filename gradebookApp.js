5487f64b14ce6d04e2ee6572606154cef0262a77
musharofahmed1 <musharofahmed11@gmail.com>
2025-06-22 15:08:33 +0800

    added getGrade function

--- gradebookApp.js
+++ gradebookApp.js
@@ -1,3 +1,4 @@
+//create getAverage function
 function getAverage(testScore){
     let totalScore = 0;
     for(let score of testScore){
@@ -6,4 +7,30 @@ function getAverage(testScore){
     return totalScore / testScore.length;
 }
 
-console.log(getAverage([56,76,54,77,90]));
+//create getGrade function
+
+function getGrade(studentScore){
+    if(studentScore==100){
+        return "A+";
+    }
+    else if(studentScore>=90 && studentScore<= 99){
+        return "A";
+    }
+    else if(studentScore>=90 && studentScore<= 99){
+        return "A";
+    }
+    else if(studentScore>=80 && studentScore<= 89){
+        return "B";
+    }
+    else if(studentScore>=70 && studentScore<= 79){
+        return "C";
+    }
+    else if(studentScore>=60 && studentScore<= 69){
+        return "D";
+    }
+    else if(studentScore>=0 && studentScore<= 59){
+        return "F";
+    }
+}
+
+console.log(getGrade(96)); //perfectly works the function
