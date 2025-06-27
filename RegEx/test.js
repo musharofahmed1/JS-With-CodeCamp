e4d9cd1bea8b9aa0a0c062a8d14f6b57b3c02a50
musharofahmed1 <musharofahmed11@gmail.com>
2025-06-27 15:31:11 +0800

    test method implemented on a string

---
+++ RegEx/test.js
@@ -0,0 +1,13 @@
+// const regex = /freeCodeCamp/;
+// const test = regex.test("e");
+// console.log(test); //false
+
+const regex = /freeCodeCamp/;
+console.log(regex.test("freeCodeCamp"));
+console.log(regex.test("freeCodeCamp is great"));
+console.log(regex.test("I love freeCodeCamp"));
+console.log(regex.test("freecodecamp"));
+console.log(regex.test("FREECODECAMP"));
+console.log(regex.test("free"));
+console.log(regex.test("code"));
+console.log(regex.test("camp"));
