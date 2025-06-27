d309d7255a97a8a51b42daad87e300b674d58635
musharofahmed1 <musharofahmed11@gmail.com>
2025-06-27 17:09:43 +0800

    tried some Modifiers

--- RegEx/gModifierInMatch.js
+++ RegEx/gModifierInMatch.js
@@ -1,4 +1,4 @@
-const regex = /freecodecamp/;
+const regex = /freecodecamp/g;
 const str = "freecodecamp is the best we love freecodecamp";
 const matched = str.match(regex);
 const replaced = str.replace(regex, "freeCodeCamp");
