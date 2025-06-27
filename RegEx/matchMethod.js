7111fd9565a42eeefe444e2d443fb6855b6644b8
musharofahmed1 <musharofahmed11@gmail.com>
2025-06-27 15:40:46 +0800

    replaced method test code added

---
+++ RegEx/matchMethod.js
@@ -0,0 +1,14 @@
+
+// const regex = /freeCodeCamp/;
+// const match = "freeCodeCamp".match(regex);
+// console.log(match);
+
+//[ 'freeCodeCamp', index: 0, input: 'freeCodeCamp', groups: undefined ]
+
+const regex = /freeCodeCamp/;
+console.log("freeCodeCamp".match(regex));  
+console.log("freeCodeCamp is great".match(regex));  
+console.log("I love freeCodeCamp".match(regex)); 
+console.log("freecodecamp".match(regex));  
+console.log("FREECODECAMP".match(regex));  
+console.log("free".match(regex)); 
---
+++ RegEx/replaceMethod.js
@@ -0,0 +1,4 @@
+const regex = /freecodecamp/;
+const str = "freecodecamp is rly kewl";
+const replaced = str.replace(regex, "freeCodeCamp");
+console.log(replaced);
