58bce5fd7132eaf9c4dc626ab4c600b717ad9f7d
musharofahmed1 <musharofahmed11@gmail.com>
2025-06-29 15:44:39 +0800

    Form Validation Learning Started

---
+++ Form Validation/script.js
@@ -0,0 +1,27 @@
+
+// console.log("Hello From JS")
+// const input = document.querySelector("input");
+
+// input.addEventListener("input", (e) => {
+//   console.log(e.target.checkValidity())
+// })
+
+// const input = document.querySelector("input");
+
+// input.addEventListener("input", (e) => {
+//   if (!e.target.checkValidity()) {
+//     e.target.reportValidity();
+//   }
+// })
+
+// const input = document.querySelector("input");
+
+// input.addEventListener("input", (e) => {
+//   if (!e.target.checkValidity()) {
+//     e.target.setCustomValidity(
+//       "You must use a company email address that ends in @sampleCompany.com"
+//     );
+//   }
+// });
+
+// Select the email input field
