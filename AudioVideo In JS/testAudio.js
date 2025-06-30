5d73218e31f1df987e622547aec1c9fbe2138db4
musharofahmed1 <musharofahmed11@gmail.com>
2025-06-30 23:50:44 +0800

    Music Player Project Setup Done

---
+++ AudioVideo In JS/testAudio.js
@@ -0,0 +1,9 @@
+const audio = document.getElementById('audio');
+
+// Starts playing the audio
+audio.play();
+
+function pauseAudio() {
+  const audio = document.getElementById('myAudio');
+  audio.pause();  // Pauses the audio playback
+}
