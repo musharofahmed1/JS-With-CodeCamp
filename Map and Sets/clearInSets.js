abe407972723aa5aaa7b7aeccf4c2fbf63600df4
musharofahmed1 <musharofahmed11@gmail.com>
2025-07-01 19:28:19 +0800

    added multiple function on Sets Practice

---
+++ Map and Sets/clearInSets.js
@@ -0,0 +1,13 @@
+const treeSet = new Set();
+
+// Add items to the treeSet
+treeSet.add('Baobab');
+treeSet.add('Jackalberry');
+treeSet.add('Mopane Tree');
+treeSet.add('Breadfruit');
+
+treeSet.delete('Breadfruit');
+
+treeSet.clear();
+
+console.log(treeSet); 
