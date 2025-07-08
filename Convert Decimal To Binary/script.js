f2cbe995214c7a9003790fe32cbb374bfe35fd40
musharofahmed1 <musharofahmed11@gmail.com>
2025-07-08 12:02:51 +0800

    Updated to 67 Steps

--- Convert Decimal To Binary/script.js
+++ Convert Decimal To Binary/script.js
@@ -3,25 +3,18 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  let binary = "";
 
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
+  if (input === 0) {
+    binary = "0";
   }
 
-  console.log("Inputs: ", inputs);
-  console.log("Quotients: ", quotients);
-  console.log("Remainders: ", remainders);
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
 
-  
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
