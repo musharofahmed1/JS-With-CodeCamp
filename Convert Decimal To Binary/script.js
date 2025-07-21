6e156c39539f93a43af3f2c9b09f83482efcc0bd
musharofahmed1 <musharofahmed11@gmail.com>
2025-07-21 23:12:24 +0800

    Decimal To Binary Converter Updated

--- Convert Decimal To Binary/script.js
+++ Convert Decimal To Binary/script.js
@@ -47,17 +47,9 @@ const showAnimation = () => {
     }, obj.addElDelay);
 
     setTimeout(() => {
-      document.getElementById(obj.inputVal).textContent = obj.msg;
+document.getElementById(obj.inputVal).textContent = obj.msg;
     }, obj.showMsgDelay);
-
-    setTimeout(() => {
-      document.getElementById(obj.inputVal).remove();
-    }, obj.removeElDelay);
   });
-
-  setTimeout(() => {
-        result.textContent = decimalToBinary(5);
-  }, 20000);
 };
 
 const checkUserInput = () => {
